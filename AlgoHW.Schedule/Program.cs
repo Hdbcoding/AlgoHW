df8c0a6ee8c03cdf9e05aba88476410b3032f8ff
henry <henry.byrd@claricode.com>
2019-03-07 21:52:47 -0500

    Schedule calculates the correct values

--- AlgoHW.Schedule/Program.cs
+++ AlgoHW.Schedule/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using AlgoHW.ScheduleLib;
 
 namespace AlgoHW.Schedule
 {
@@ -6,7 +7,8 @@ namespace AlgoHW.Schedule
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var times = ScheduleLoader.CalculateWeightedCompletionTime("schedule_data.txt");
+            Console.WriteLine($"poor: {times.poor}, optimal: {times.optimal}");
         }
     }
 }
